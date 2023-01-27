93121e066811a9dcd260bfe9b9ed0a1a0eb8f070
Serhii Ilnytskyi <serhiiilnytskyi@gmail.com>
2023-01-27 22:21:14 +0200

    [issue-18] Make fixes according review comments

--- src/main/java/com/bringframework/annotations/Component.java
+++ src/main/java/com/bringframework/annotations/Component.java
@@ -3,6 +3,7 @@ package com.bringframework.annotations;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
  * Annotation used to mark a class as a bean that can be managed by an IoC container. The class will be instantiated and
  * configured by the container, and its lifecycle will be managed.
  */
+@Documented
 @Target({TYPE})
 @Retention(RUNTIME)
 public @interface Component {
