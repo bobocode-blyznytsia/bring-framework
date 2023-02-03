a53e6c7336dfa6f8a2fe00f2a3a196be1c278f84
Maksym Voronov <maksvoronov@gmail.com>
2023-02-03 18:32:53 +0200

    [issue-#21] Add logging information

--- src/main/java/com/bringframework/factory/RawBeanProcessor.java
+++ src/main/java/com/bringframework/factory/RawBeanProcessor.java
@@ -5,6 +5,7 @@ import static com.bringframework.util.BeanUtils.createInstance;
 import com.bringframework.registry.BeanDefinition;
 import com.bringframework.registry.BeanDefinitionRegistry;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Creates instances of beans by provided {@link BeanDefinition}s from
@@ -12,6 +13,7 @@ import java.util.Map;
  * RawBeanProcessor is not responsible for injecting other beans,
  * but {@link AutowiredBeanPostProcessor}.
  */
+@Slf4j
 public class RawBeanProcessor {
 
   private final Map<String, Object> rawBeanMap;
@@ -25,9 +27,20 @@ public class RawBeanProcessor {
   }
 
   public void process() {
-    this.beanDefinitionRegistry.getAllBeanDefinitions()
-        .forEach((beanName, beanDefinition) -> rawBeanMap.put(beanName,
-            createInstance(beanDefinition.getBeanClass())));
+    Map<String, BeanDefinition> beanDefinitions =
+        this.beanDefinitionRegistry.getAllBeanDefinitions();
+    log.debug("Started creating {} beans without dependencies found in bean definition registry.",
+        beanDefinitions.size());
+    beanDefinitions.forEach(this::initializeBean);
+    log.debug("Finished creating beans.");
+  }
+
+  private void initializeBean(String beanName, BeanDefinition beanDefinition) {
+    Class<Object> beanClass = beanDefinition.getBeanClass();
+    Object bean = createInstance(beanClass);
+    rawBeanMap.put(beanName, bean);
+    log.debug("Bean with name {} of type {} has been created.", beanName,
+        beanClass.getSimpleName());
   }
 
 }
