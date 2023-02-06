e89e0cf05e466c603902745edb1928ca821d7dcc
Mykola_Harasym <mykola.harasym@spglobal.com>
2023-02-06 09:06:58 +0200

    [issue-#21] resolve comments

--- src/main/java/com/bringframework/factory/impl/BeanFactoryImpl.java
+++ src/main/java/com/bringframework/factory/impl/BeanFactoryImpl.java
@@ -1,12 +1,13 @@
 package com.bringframework.factory.impl;
 
 import com.bringframework.factory.BeanFactory;
+import com.bringframework.factory.BeanPostProcessor;
 import com.bringframework.factory.RawBeanProcessor;
 import com.bringframework.registry.BeanDefinitionRegistry;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * This class is the main implementation of the {@link BeanFactory} interface.
@@ -14,14 +15,11 @@ import lombok.Setter;
 public class BeanFactoryImpl implements BeanFactory {
   private final Map<String, Object> rawBeansMap = new ConcurrentHashMap<>();
   private final BeanDefinitionRegistry beanDefinitionRegistry;
-  //private List<BeanPostProcessor> beanPostProcessorList;
-
-  @Getter
-  @Setter
-  private RawBeanProcessor rawBeanProcessor;
+  private final RawBeanProcessor rawBeanProcessor;
 
   public BeanFactoryImpl(BeanDefinitionRegistry beanDefinitionRegistry) {
     this.beanDefinitionRegistry = beanDefinitionRegistry;
+    rawBeanProcessor = new RawBeanProcessor(this.beanDefinitionRegistry, rawBeansMap);
   }
 
   /**
@@ -29,10 +27,9 @@ public class BeanFactoryImpl implements BeanFactory {
    */
   @Override
   public Map<String, Object> createBeans() {
-    setRawBeanProcessor(new RawBeanProcessor(beanDefinitionRegistry, rawBeansMap));
     rawBeanProcessor.process();
-    // TODO: 2/4/2023 create list of bean post processor and process raw beans
-    //beanPostProcessorList.forEach(BeanPostProcessor::process);
+    List<BeanPostProcessor> beanPostProcessorList = new ArrayList<>();
+    beanPostProcessorList.forEach(BeanPostProcessor::process);
     return rawBeansMap;
   }
 }
