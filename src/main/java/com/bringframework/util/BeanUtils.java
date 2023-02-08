06344c5f09a03f772458553768b5d74ddba02ee0
Mykola_Harasym <mykola.harasym@spglobal.com>
2023-02-08 10:48:15 +0200

    [issue-#49] Validate package which will be scanned, add this logic to utils class

--- src/main/java/com/bringframework/util/BeanUtils.java
+++ src/main/java/com/bringframework/util/BeanUtils.java
@@ -18,4 +18,16 @@ public final class BeanUtils {
     }
   }
 
+  /**
+   * Validate package which will be scanned and beans will be searched.
+   *
+   * @param packageName the package with beans
+   */
+  public static void validatePackageName(String packageName) {
+    if (packageName == null || packageName.isBlank()) {
+      throw new IllegalArgumentException(
+          "The package name can't be empty or null. Please specify the correct package name.");
+    }
+  }
+
 }
