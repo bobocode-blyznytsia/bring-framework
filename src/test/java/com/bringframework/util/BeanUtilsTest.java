4436719161816fe6f4b0d5bb3bacc507ff501421
Oleh Haliak <aiupea5@Amway.com>
2023-02-11 10:59:14 +0200

    [issue-#28] Add unit tests for BeanUtils

--- src/test/java/com/bringframework/util/BeanUtilsTest.java
+++ src/test/java/com/bringframework/util/BeanUtilsTest.java
@@ -1,11 +1,14 @@
 package com.bringframework.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 
 import com.bringframework.exception.BeanInitializationException;
 import java.util.AbstractList;
 import java.util.List;
+import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
 class BeanUtilsTest {
@@ -47,4 +50,42 @@ class BeanUtilsTest {
   void shouldThrowExceptionWhenPackageNameIsEmpty() {
     assertThrows(IllegalArgumentException.class, () -> BeanUtils.validatePackageName("   "));
   }
+
+  @Test
+  void shouldCreateInstanceWithNoArgsFactoryMethod() throws ReflectiveOperationException {
+    var factoryMethod = TestConfiguration.class.getDeclaredMethod("factoryMethod");
+    assertSame(TestConfiguration.MOCK_OBJECT, BeanUtils.createInstance(factoryMethod));
+  }
+
+  @Test
+  void shouldCreateInstanceWithFactoryMethodWithArgs() throws ReflectiveOperationException {
+    var factoryMethod = TestConfiguration.class.getDeclaredMethod("factoryMethodWithParam", Object.class);
+    var mockParam = mock();
+    assertSame(mockParam, BeanUtils.createInstance(factoryMethod, mockParam));
+  }
+
+  @Test
+  void shouldWrapExceptionIfFactoryMethodFails() throws ReflectiveOperationException{
+    var factoryMethod = TestConfiguration.class.getDeclaredMethod("failingFactoryMethod");
+    assertThrows(BeanInitializationException.class,() -> BeanUtils.createInstance(factoryMethod));
+  }
+  @NoArgsConstructor
+  static class TestConfiguration {
+
+    private static final Object MOCK_OBJECT = new Object();
+
+    Object factoryMethod() {
+      return MOCK_OBJECT;
+    }
+
+    Object factoryMethodWithParam(Object obj) {
+      return obj;
+    }
+
+    Object failingFactoryMethod(){
+      throw new RuntimeException("It's not your day, mate. You've got an exception!");
+    }
+
+  }
+
 }
