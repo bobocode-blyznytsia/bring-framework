5aa4076262fcdf8cb260f7df0f8e27a55d77b87f
roman <rhrupskyi@exoft.net>
2023-02-11 18:51:21 +0200

    [issue-#28] code review items

--- src/main/java/com/bringframework/exception/BeanDefinitionDuplicateNameException.java
+++ src/main/java/com/bringframework/exception/BeanDefinitionDuplicateNameException.java
@@ -10,7 +10,7 @@ import com.bringframework.registry.BeanDefinitionRegistry;
 public class BeanDefinitionDuplicateNameException extends BringException {
 
   private static final String DEFAULT_BEAN_DEFINITION_DUPLICATE_NAME_EXCEPTION_MESSAGE =
-      "BeanDefinition with name %s already exists!";
+      "BeanDefinition with name %s already exists";
 
   public BeanDefinitionDuplicateNameException(String beanName) {
     super(DEFAULT_BEAN_DEFINITION_DUPLICATE_NAME_EXCEPTION_MESSAGE.formatted(beanName));
