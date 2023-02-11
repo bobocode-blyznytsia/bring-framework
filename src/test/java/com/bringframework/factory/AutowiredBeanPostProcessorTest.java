cc1bd140bd832dd306ce83f6c55cdccbf465c478
Oleh Haliak <aiupea5@Amway.com>
2023-02-11 11:13:54 +0200

    [issue-#28] Fix AutowiredBPP test

--- src/test/java/com/bringframework/factory/AutowiredBeanPostProcessorTest.java
+++ src/test/java/com/bringframework/factory/AutowiredBeanPostProcessorTest.java
@@ -1,5 +1,6 @@
 package com.bringframework.factory;
 
+import static java.util.Collections.emptyMap;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -8,9 +9,7 @@ import com.bringframework.registry.BeanDefinition;
 import com.bringframework.registry.BeanDefinitionRegistry;
 import com.bringframework.resolver.DependencyResolver;
 import java.lang.reflect.Field;
-import java.util.Collections;
 import java.util.Map;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -24,50 +23,41 @@ class AutowiredBeanPostProcessorTest {
 
   @Mock
   private DependencyResolver dependencyResolver;
-
   @Mock
   private BeanDefinitionRegistry beanDefinitionRegistry;
-
-  private AutowiredBeanPostProcessor autowiredBPP;
 
   @Test
   void injectsIntoField() throws NoSuchFieldException {
     var testClassABean = new TestClassA();
-    var secondBean = new TestClassB();
+    var testClassBBean = new TestClassB();
 
-//    var firstBeanDefinition = mock(BeanDefinition.class);
-//    when(firstBeanDefinition.getAutowiredFieldsMetadata()).thenReturn(Collections.emptyMap());
-//    when(beanDefinitionRegistry.getBeanDefinition(TEST_CLASS_A_BEAN_NAME)).thenReturn(firstBeanDefinition);
-//
-//    var secondBeanDefinition = mock(BeanDefinition.class);
-//    Field testClassAField = TestClassB.class.getDeclaredField(TEST_CLASS_A_FIELD_NAME);
-//    when(secondBeanDefinition.getAutowiredFieldsMetadata())
-//        .thenReturn(Map.of(TEST_CLASS_A_FIELD_NAME, testClassAField));
-//    when(beanDefinitionRegistry.getBeanDefinition(TEST_CLASS_B_BEAN_NAME)).thenReturn(secondBeanDefinition);
-//
-//    Map<String, Object> rawBeans = Map.of(
-//        TEST_CLASS_A_BEAN_NAME, testClassABean,
-//        TEST_CLASS_B_BEAN_NAME, secondBean
-//    );
-//
-//    when(dependencyResolver.getCandidateNameOfType(TestClassA.class)).thenReturn(TEST_CLASS_A_BEAN_NAME);
-//    autowiredBPP = new AutowiredBeanPostProcessor(beanDefinitionRegistry, rawBeans);
-//    injectMockDependencyResolver();
-//
-//    autowiredBPP.process();
-//
-//    assertSame(testClassABean, secondBean.testClassA);
+    var firstBeanDefinition = mock(BeanDefinition.class);
+    when(firstBeanDefinition.getAutowiredFieldsMetadata()).thenReturn(emptyMap());
+
+    var secondBeanDefinition = mock(BeanDefinition.class);
+    Field testClassAField = TestClassB.class.getDeclaredField(TEST_CLASS_A_FIELD_NAME);
+    when(secondBeanDefinition.getAutowiredFieldsMetadata())
+        .thenReturn(Map.of(TEST_CLASS_A_FIELD_NAME, testClassAField));
+
+    when(beanDefinitionRegistry.getBeanDefinition(TEST_CLASS_A_BEAN_NAME)).thenReturn(firstBeanDefinition);
+    when(beanDefinitionRegistry.getBeanDefinition(TEST_CLASS_B_BEAN_NAME)).thenReturn(secondBeanDefinition);
+
+    Map<String, Object> rawBeans = Map.of(
+        TEST_CLASS_A_BEAN_NAME, testClassABean,
+        TEST_CLASS_B_BEAN_NAME, testClassBBean
+    );
+    Map<String, BeanDefinition> beanDefinitionMap = Map.of(
+        TEST_CLASS_A_BEAN_NAME, firstBeanDefinition,
+        TEST_CLASS_B_BEAN_NAME, secondBeanDefinition
+    );
+
+    when(beanDefinitionRegistry.getAllBeanDefinitions()).thenReturn(beanDefinitionMap);
+    when(dependencyResolver.getCandidateNameOfType(TestClassA.class)).thenReturn(TEST_CLASS_A_BEAN_NAME);
+
+    new AutowiredBeanPostProcessor(beanDefinitionRegistry, rawBeans,dependencyResolver).process();
+
+    assertSame(testClassABean, testClassBBean.testClassA);
   }
-
-  @SneakyThrows
-  private void injectMockDependencyResolver() {
-    Field dependencyResolverField =
-        autowiredBPP.getClass().getDeclaredField("dependencyResolver");
-    dependencyResolverField.setAccessible(true);
-    dependencyResolverField.set(autowiredBPP, dependencyResolver);
-    dependencyResolverField.setAccessible(false);
-  }
-
 
   class TestClassA {
 
